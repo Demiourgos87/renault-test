c6369592e78e7066db96aede53b4c9a6d539960d
Bojan Knezevic <bojan.knezevic@gmail.com>
2017-06-20 16:11:20 +0200

    Finish Portrait slider for mob

---
+++ js/portraitSlider.js
@@ -0,0 +1,43 @@
+(function($){
+  // FAQ Slider \\
+
+  // cache DOM \\
+  var $sliderWrap = $('.portrait__slider'),
+      $slider = $sliderWrap.find('.slider'),
+      $slides = $slider.find('.portrait__slide'),
+      $navigation = $sliderWrap.find('.slider__navigation'),
+      $arrowNext = $navigation.find('.fa-caret-right'),
+      $arrowPrev = $navigation.find('.fa-caret-left'),
+      $slidesTotal = $navigation.find('.slide__total'),
+      $currentSlide = $navigation.find('.slide__current'),
+      totalSlides = $slides.length;
+
+  $slidesTotal.text('0' + totalSlides);
+
+  function updateNavigationCounter(currentIndex) {
+
+    $currentSlide.text('0' + (currentIndex + 1));
+
+  }
+
+  $slider.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    speed: 400,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    dots: false,
+    infinite: true,
+    prevArrow: $arrowPrev,
+    nextArrow: $arrowNext
+  });
+
+  $slider.on('afterChange', function(event, slick, currentSlide) {
+
+    updateNavigationCounter(currentSlide);
+
+  });
+
+})(jQuery);
