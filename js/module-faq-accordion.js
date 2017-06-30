469c4a0be59da2d0a5a2a6b44af02af33374a53b
Bojan Knezevic <bojan.knezevic@gmail.com>
2017-06-30 12:58:51 +0200

    Expand ACCORDION module functionality

--- js/module-faq-accordion.js
+++ js/module-faq-accordion.js
@@ -8,22 +8,42 @@
       $accordion = $accordionWrap.find('.module-faq-accordion'),
       $accordionElements = $accordion.find('.accordion__element');
 
+  function closeOpenedElement() {
+
+      var $opened = $accordion.find('.accordion__element.opened'),
+          $openedText = $opened.find('.accordion__element__text'),
+          $openedArrow = $opened.find('.accordion-heading__arrow i');
+
+      $opened.removeClass('opened');
+      $openedText.slideUp(200);
+      $openedArrow.removeClass('fa-caret-up').addClass('fa-caret-down');
+
+  }
+
   $.each($accordionElements, function() {
 
-    var $heading = $(this).find('.accordion__element__heading');
+    var $current = $(this);
+        $heading = $current.find('.accordion__element__heading');
 
     $heading.on('click', function() {
 
       var $arrow = $(this).find('.accordion-heading__arrow i'),
           $accordionText = $(this).next('.accordion__element__text');
 
-      if ($arrow.hasClass('fa-caret-down')) {
-        $arrow.removeClass('fa-caret-down').addClass('fa-caret-up');
-      } else {
-        $arrow.removeClass('fa-caret-up').addClass('fa-caret-down');
-      }
+      if ($current.hasClass('opened')) {
 
-      $accordionText.slideToggle(200);
+        $current.removeClass('opened');
+        $accordionText.slideUp(200);
+        $arrow.removeClass('fa-caret-up').addClass('fa-caret-down');
+
+      } else {
+
+        closeOpenedElement();
+        $current.addClass('opened');
+        $accordionText.slideDown(200);
+        $arrow.removeClass('fa-caret-down').addClass('fa-caret-up');
+
+      }
 
     });
 
