cb6fe94b1e36da3152d9890e148e274e70e6aca3
Bojan Knezevic <bojan.knezevic@gmail.com>
2017-08-22 13:37:41 +0200

    Adjust DIAPORAMA in ARTICLE PORTRAIT

--- app/js/article-social-share.js
+++ app/js/article-social-share.js
@@ -1,24 +1,27 @@
 (function($){
-  var article = $('.article-body__body'),
-      headerHeight = $('.b-header').outerHeight(),
-      articleHeight = article.outerHeight(),
-      articleTopPosition = article.offset().top - headerHeight,
-      articleBottomPosition = articleTopPosition + articleHeight,
-      social = $('.c-social-share.article-default'),
-      socialHeight = social.outerHeight();
+  var article = $('body').find('.article-body__body'),
+      social = $('.c-social-share.article-default');
+  if (social.length > 0) {
+    var headerHeight = $('.b-header').outerHeight(),
+        articleHeight = article.outerHeight(),
+        articleTopPosition = article.offset().top - headerHeight,
+        articleBottomPosition = articleTopPosition + articleHeight,
+        social = $('.c-social-share.article-default'),
+        socialHeight = social.outerHeight();
 
-  $(window).on('scroll', function(){
-    var windowTopPosition = $(window).scrollTop() + 10;
-    socialTopPosition = social.offset().top - headerHeight,
-    socialBottomPosition = socialTopPosition + socialHeight;
+    $(window).on('scroll', function(){
+      var windowTopPosition = $(window).scrollTop() + 10;
+      socialTopPosition = social.offset().top - headerHeight,
+      socialBottomPosition = socialTopPosition + socialHeight;
 
-    if (windowTopPosition > articleTopPosition) {
-      var offset = windowTopPosition - articleTopPosition;
-      social.animate({top: offset + 'px'}, 200);
+      if (windowTopPosition > articleTopPosition) {
+        var offset = windowTopPosition - articleTopPosition;
+        social.animate({top: offset + 'px'}, 200);
 
-      if (articleBottomPosition < (windowTopPosition + socialHeight + 60)) {
-        social.stop();
+        if (articleBottomPosition < (windowTopPosition + socialHeight + 60)) {
+          social.stop();
+        }
       }
-    }
-  });
+    });
+  }
 })(jQuery);
